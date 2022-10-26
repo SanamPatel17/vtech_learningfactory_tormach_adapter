3c7f230e5a20e93580f680873e38c60ddfb66ec0
Sanam Patel <66695125+SanamPatel17@users.noreply.github.com>
2022-10-26 15:08:33 -0400

    Update Tormach PCNC 1100 Adapter.py

---
+++ Tormach PCNC 1100 Adapter.py
@@ -0,0 +1,131 @@
+import threading
+import time
+import socket
+import sys
+import datetime
+import serial
+import re
+import requests
+
+client_counter = 0
+client_list = []
+first_run_flag = 1
+lock = threading.Lock()
+event = threading.Event()
+event.set()
+
+# Initialising # global attributes for HAAS serial comm macros
+*** mac_status = part_num = prog_name = sspeed = coolant = sload = cut_status = combined_output = 'Nil'
+
+"""Creating Socket Objects"""
+HOST = 'localhost'
+PORT = 7878
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+"""Binding to the local port/host"""
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+
+"""Start Listening to Socket for Clients"""
+s.listen(5)
+
+"""Function to Clear Out Threads List Once All Threads are Empty"""
+
+
+def thread_list_empty():
+    global client_list, client_counter
+
+    while True:
+        try:
+            if client_counter == 0 and first_run_flag == 0 and client_list != []:
+                print("%d Clients Active" % client_counter)
+                print("Clearing All threads....")
+                for index, thread in enumerate(client_list):
+                    thread.join()
+                client_list = []
+        except:
+            print("Invalid Client List Deletion")
+
+
+"""Function that parses attributes from the Tormach"""
+
+
+def fetch_from_Tormach():
+
+
+*** *** ***
+
+*** *** ***
+
+"""Main Thread Class For Clients"""
+
+
+class NewClientThread(threading.Thread):
+    # init method called on thread object creation,
+    def __init__(self, conn, string_address):
+        threading.Thread.__init__(self)
+        self.connection_object = conn
+        self.client_ip = string_address
+
+    # run method called on .start() execution
+    def run(self):
+        global client_counter, combined_output
+        global lock
+        while True:
+            try:
+                #print("Sending data to Client {} in {}".format(self.client_ip, self.getName()))
+                out = combined_output
+                print("OUT: " + out)
+                self.connection_object.sendall(out.encode())
+                time.sleep(0.5)
+
+            except err:
+                lock.acquire()
+                try:
+                    print(err)
+                    client_counter = client_counter - 1
+                    print("Connection disconnected for ip {} ".format(
+                        self.client_ip))
+                    break
+                finally:
+                    lock.release()
+
+
+"""Starts From Here"""
+t1 = threading.Thread(target=thread_list_empty)
+t2 = threading.Thread(target=fetch_from_HAAS)
+t1.setDaemon(True)
+t2.setDaemon(True)
+t1.start()
+t2.start()
+time.sleep(2)
+
+while event.is_set():
+
+    if first_run_flag == 1:
+        print("Listening to Port: %d...." % PORT)
+
+    try:
+        conn, addr = s.accept()
+        lock.acquire()
+        client_counter = client_counter + 1
+        first_run_flag = 0
+        print("Accepting Comm From:" + " " + str(addr))
+        new_Client_Thread = NewClientThread(conn, str(addr))
+        new_Client_Thread.setDaemon(True)
+        client_list.append(new_Client_Thread)
+        print(client_list)
+        new_Client_Thread.start()
+        lock.release()
+    except KeyboardInterrupt:
+        print("\nExiting Program")
+        sys.exit()
+
+if not event.is_set():
+    print("\nExiting Program")
+    sys.exit()
